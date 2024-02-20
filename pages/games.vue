c94ad58390ec798a2bd8eba0744fbe5c12269d73
luddthree~ <luddetv@gmail.com>
2024-02-20 12:42:43 +0100

    Remove app.vue and update package.json and nuxt.config.ts

---
+++ pages/games.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-center font-bold mt-3">
+        <NuxtLink to="/">
+            <img src="~/public/vorsspill.png" alt="logo" class="w-1/4 h-1/4 mx-auto">
+        </NuxtLink>
+    </div>
+  
+    <div class=" text-center mt-8 ">
+      <button class=" mt-4 transition ease-in-out delay-50 bg-blue-500 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-50 ...">spill 1</button>
+  
+      <button class=" mt-4 transition ease-in-out delay-50 bg-blue-500 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-50 ...">spill 2</button>
+  
+      <button class=" mt-4 transition ease-in-out delay-50 bg-blue-500 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-50 ...">spill 3</button>
+  
+      <button class=" mt-4 transition ease-in-out delay-50 bg-blue-500 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-50 ...">spill 4</button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+import type { NuxtLink } from '#build/components';
+
+  
+  </script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="">
+    <NuxtLink to="/games">
+        <img src="/vorsspill.png" alt="logo" class="w-2/4 h-2/4 mx-auto">
+    </NuxtLink>
+</div>
+
+<div class="">
+
+</div>
+
+
+</template>
+
+
+<script lang="ts">
+import type { NuxtLink } from '#build/components';
+
+
+</script>
