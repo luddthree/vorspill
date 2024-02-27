0621b9572639c44bdeec5e9f8782530c59a71f2d
luddthree~ <luddetv@gmail.com>
2024-02-27 08:52:10 +0100

    Update game buttons and routes

--- pages/games.vue
+++ pages/games.vue
@@ -6,17 +6,17 @@
     </div>
   
     <div class=" text-center mt-8 ">
-    <button  @click="spill1()" class="rounded mt-4 rotate-1 text-white transition ease-in-out delay-50 bg-orange-500 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-orange-600 duration-50 ...">spill 1</button>
+    <button  @click="spill1()" class="rounded mt-4 rotate-1 text-white transition ease-in-out delay-50 bg-orange-500 py-3 px-20 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-orange-600 duration-50 ...">Gjør elr drikk</button>
   
-      <button @click="spill2()" class="rounded mt-4 -rotate-1 text-white transition ease-in-out delay-50 bg-red-600 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-red-700 duration-50 ...">spin</button>
+      <button @click="spill2()" class="rounded mt-4 -rotate-1 text-white transition ease-in-out delay-50 bg-red-600 py-3 px-24 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-red-700 duration-50 ...">flasketuten</button>
   
-      <button @click="spill3()" class="rounded text-white mt-4 rotate-1 transition ease-in-out delay-50 bg-yellow-400 py-3 px-20 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-yellow-500 duration-50 ...">Hvem i rommet</button>
+      <button @click="spill3()" class="rounded text-white mt-4 rotate-1 transition ease-in-out delay-50 bg-yellow-400 py-3 px-24 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-yellow-500 duration-50 ...">Pekeleken</button>
   
-      <button class="rounded text-white mt-4 rotate-0 transition ease-in-out delay-50 bg-lime-500 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-lime-600 duration-50 ...">spill 4</button>
+      <button class="rounded text-white mt-4 rotate-0 transition ease-in-out delay-50 bg-lime-500 py-3 px-20 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-lime-600 duration-50 ...">Jeg har aldri</button>
 
-      <button class="rounded text-white mt-4 -rotate-1 transition ease-in-out delay-50 bg-teal-500 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-teal-600 duration-50 ...">spill 5</button>
+      <button class="rounded text-white mt-4 -rotate-1 transition ease-in-out delay-50 bg-teal-500 py-3 px-28 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-teal-600 duration-50 ...">spill 5</button>
 
-      <button class="rounded text-white mt-4 rotate-1 transition ease-in-out delay-50 bg-blue-500 py-3 px-32 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-50 ...">spill 6</button>
+      <button class="rounded text-white mt-4 rotate-1 transition ease-in-out delay-50 bg-blue-500 py-3 px-28 text-2xl hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-50 ...">spill 6</button>
 
     </div>
 
@@ -51,9 +51,9 @@ async function spill1() {
 
 async function spill2() {
   const originalBackgroundColor = document.body.style.backgroundColor;
-  navigateTo('/spill/flasketut/question');
+  navigateTo('/spill/flasketut/spm');
   let bg = 0;
-  while (bg < 10) {
+  while (bg < 20) {
     document.body.style.backgroundColor = generateRandomColor();
     await sleep(500);
     bg++;
@@ -64,7 +64,7 @@ async function spill2() {
 
 async function spill3() {
   const originalBackgroundColor = document.body.style.backgroundColor;
-  navigateTo('/spill/flasketut/question');
+  navigateTo('/spill/pekeleken/spm');
   let bg = 0;
   while (bg < 10) {
     document.body.style.backgroundColor = generateRandomColor();
@@ -72,7 +72,7 @@ async function spill3() {
     bg++;
   }
   document.body.style.backgroundColor = originalBackgroundColor;
-  navigateTo('/spill/flasketut/spin');
+  navigateTo('/spill/pekeleken/spin');
 }
 
 
--- pages/spill/flasketut/spin.vue
+++ pages/spill/flasketut/spin.vue
@@ -8,14 +8,28 @@
   import { ref, onMounted } from 'vue';
   
   const spinStyle = ref({});
+  const initialSpins = 4080; // 3 full spins (360 degrees * 3)
   
   onMounted(() => {
-    const fastSpinDuration = 10; // Fast spin duration in seconds for the initial spins
+    const fastSpinDuration = 8; // Fast spin duration in seconds for the initial spins
+    const randomExtraSpin = Math.floor(Math.random() * 360 + 3000); // Random angle for the extra spin
+    const totalSpin = initialSpins + randomExtraSpin; // Total spin including the initial spins and the random extra spin
   
     spinStyle.value = {
       animation: `spin ${fastSpinDuration}s forwards, final-spin 2s ease-out forwards`,
       animationDelay: `0s, ${fastSpinDuration}s`,
     };
+  
+    // Define keyframes for the final spin dynamically
+    const styleSheet = document.createElement('style');
+    styleSheet.type = 'text/css';
+    styleSheet.innerHTML = `
+      @keyframes spin {
+        from { transform: rotate(${initialSpins}deg); }
+        to { transform: rotate(${totalSpin}deg); }
+      }
+    `;
+    document.head.appendChild(styleSheet);
   });
   </script>
   
@@ -27,5 +41,5 @@
   }
   
 
-  </style>
+  </style> */
   
