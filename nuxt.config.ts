8384e0112c095c8c14891fcf0dd7c2d8cfcdca5d
luddthree~ <luddetv@gmail.com>
2024-04-10 13:08:59 +0200

    Add Google AdSense script to app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8921164054573762',
+          async: true,
+          crossorigin: 'anonymous'
+        }
+      ]
+    }
+  },
   runtimeConfig: {
     public: {
       gtagId: "G-XBCERVVBG8",
