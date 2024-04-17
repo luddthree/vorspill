1e36b997be990eb62af10ded53cfb64a8549b231
luddthree~ <luddetv@gmail.com>
2024-04-17 10:39:14 +0200

    tos

---
+++ pages/tos.vue
@@ -0,0 +1,29 @@
+<template>
+<h1 class="shead">Privacy Policy for Vorspill</h1>
+<p class="stext mt-5">Effective Date: 17.04.2024</p>
+
+<p class="mt-8 stext">Welcome to Vorspill! We respect your privacy and are committed to protecting it through our compliance with this policy. This policy describes the types of information we may collect from you or that you may provide when you use the Vorspill app and our practices for collecting, using, maintaining, protecting, and disclosing that information.</p>
+
+<h1 class="shead mt-8">1. Information We Collect About You
+</h1>
+<p class="stext mt-5">Google Analytics for Web: We do not collect any personal information within the Vorspill app. However, for our website, we use Google Analytics to collect aggregated data such as how many people visit our site, the pages they visit, and where they are coming from. This data is strictly used to improve the functionality and user experience of our website. Google Analytics may capture your IP address, but no other personal information is captured.</p>
+
+<h1 class="shead mt-8">2. How We Use Your Information
+</h1>
+<p class="stext mt-5">The information we collect through Google Analytics is used to:</p>
+
+<div class="text-white text-center">
+<li>Understand and analyze the usage trends and preferences of our visitors and users.</li>
+<li>Improve our website to better serve you.</li>
+<li>Monitor and analyze the effectiveness of our marketing campaigns.</li>
+</div>
+
+<h1 class="shead mt-8">3. How We Share Your Information
+</h1>
+<p class="stext mt-5">We do not share your personal information with any third parties, except as necessary to maintain and analyze the performance of our website through Google Analytics.</p>
+
+<h1 class="shead mt-8">4. Data Security
+</h1>
+<p class="stext mt-5">We implement a variety of security measures designed to maintain the safety of your personal information when you visit our website. However, the transmission of information via the internet is not completely secure. Although we do our</p>
+
+</template>
